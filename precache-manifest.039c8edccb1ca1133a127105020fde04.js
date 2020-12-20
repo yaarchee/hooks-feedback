4699dace375ce2d359997143fd0f7b936a18f6b9
Yaroslav Paltsun <trancemanjake@gmail.com>
2020-12-20 18:40:46 +0200

    Updates

---
+++ precache-manifest.039c8edccb1ca1133a127105020fde04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9bf176473b3da0bad570bb14f241dbf",
+    "url": "/hooks-feedback/index.html"
+  },
+  {
+    "revision": "4b4d2e91a2e376a3a277",
+    "url": "/hooks-feedback/static/css/main.e43a7f3d.chunk.css"
+  },
+  {
+    "revision": "c57e126f92514f3686bf",
+    "url": "/hooks-feedback/static/js/2.2acef6e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hooks-feedback/static/js/2.2acef6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b4d2e91a2e376a3a277",
+    "url": "/hooks-feedback/static/js/main.3b3ad17c.chunk.js"
+  },
+  {
+    "revision": "d95cff45cfbd41a38048",
+    "url": "/hooks-feedback/static/js/runtime-main.7cafb40b.js"
+  }
+]);
